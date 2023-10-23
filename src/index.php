346601d584aa30a9956d07485f72b236be42d017
Karen <103889430+karen-nke@users.noreply.github.com>
2023-10-23 16:20:36 +0800

    Homepage mockup

--- src/index.php
+++ src/index.php
@@ -1,5 +1,166 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Your Website Title</title>
+</head>
 
-echo "testing"
-?>
+<style>
+    body {
+    width: 100%;
+    min-height: 100vh;
+    background: #F8F8FA;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 
+.container {
+    max-width: 1300px;
+    margin: auto;
+    padding-left: 25px;
+    padding-right: 25px;
+    padding-top: 100px; /* Add some padding to the top */
+    height: 100vh;
+   
+}
+
+
+
+    .centered-container {
+    text-align: center;
+    }
+
+    .custom-button {
+        border: none;
+        border-radius: 5px;
+        background-color: #1D86C5;
+        color: white;
+        padding: 10px 20px;
+        font-size: 16px;
+        cursor: pointer;
+    }
+
+    .club-container{
+        max-width: 1500px;
+        margin: auto;
+        padding-left: 5px;
+        padding-right: 5px;
+
+    } 
+    
+    .club-container .column {
+  float: left;
+  width: 25%;
+  padding: 10px;
+  min-width: 200px;
+  margin-bottom: 50px;
+  transition: transform 0.5s;
+}
+
+/* Clearfix (clear floats) */
+.club-container .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title{
+  text-align: center;
+  margin: 0px auto 80px;
+  position: relative;
+  line-height: 60px;
+  color: #555;
+}
+.title::after{
+  content: '';
+  background: #a3a3a3;
+  width: 80px;
+  height: 5px;
+  border-radius: 5px;
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.club-container .btn{
+  display: block;
+  width: 100%;
+  padding: 15px 20px;
+  text-align: center;
+  border: none;
+  background: #376189;
+  outline: none;
+  border-radius: 30px;
+  font-size: 1.2rem;
+  cursor: pointer;
+  transition: .3s;
+}
+
+.club-container .btn a{
+  color:white;
+}
+</style>
+
+
+<body>
+    <div class="container">
+
+    <img style="width: 80%%; height: 80%" src="https://via.placeholder.com/948x478" />
+
+
+        <div style="width: 100%; height: 100%; justify-content: center; align-items: center; display: inline-flex">
+            <div style="width: 1175px; height: 503px; position: relative">
+                <div style="width: 378.33px; height: 400px; left: 0px; top: 103px; position: absolute; background: white; box-shadow: 0px -0.274791419506073px 2.7479140758514404px rgba(0, 0, 0, 0.02); border-radius: 10px; overflow: hidden">
+                    <div style="width: 338px; height: 240px; left: 20px; top: 20px; position: absolute; border-radius: 5px; justify-content: center; align-items: center; display: inline-flex">
+                        <img style="width: 338px; height: 240px; border-radius: 5px" src="https://via.placeholder.com/338x240" />
+                    </div>
+                    <div style="width: 338px; left: 20px; top: 275px; position: absolute; color: black; font-size: 16px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">Yoga Class to freshen up the day</div>
+                    <div style="width: 338px; left: 20px; top: 329px; position: absolute; color: #1D86C5; font-size: 12px; font-family: Open Sans; font-weight: 400; word-wrap: break-word"> Friday, 13th October 2023 6.00 - 8:30PM</div>
+                    <div style="width: 338px; left: 20px; top: 365px; position: absolute; color: #7E7E7E; font-size: 12px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">Sunway University, Art Gallery</div>
+                </div>
+                <div style="width: 378.33px; height: 400px; left: 398.33px; top: 103px; position: absolute; background: white; box-shadow: 0px -0.274791419506073px 2.7479140758514404px rgba(0, 0, 0, 0.02); border-radius: 10px; overflow: hidden">
+                    <div style="width: 338px; height: 240px; left: 20px; top: 20px; position: absolute; border-radius: 5px; justify-content: center; align-items: center; display: inline-flex">
+                        <img style="width: 338px; height: 240px; border-radius: 5px" src="https://via.placeholder.com/338x240" />
+                    </div>
+                    <div style="width: 338px; left: 20px; top: 275px; position: absolute; color: black; font-size: 16px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">Hiking Session with Sunway Environmental Society</div>
+                    <div style="width: 338px; left: 20px; top: 329px; position: absolute; color: #1D86C5; font-size: 12px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">Saturday, March 18, 9.30PM</div>
+                    <div style="width: 338px; left: 20px; top: 365px; position: absolute; color: #7E7E7E; font-size: 12px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">ONLINE EVENT - Attend anywhere</div>
+                </div>
+                <div style="width: 378.33px; height: 400px; left: 796.67px; top: 103px; position: absolute; background: white; box-shadow: 0px -0.274791419506073px 2.7479140758514404px rgba(0, 0, 0, 0.02); border-radius: 10px; overflow: hidden">
+                    <div style="width: 338px; height: 240px; left: 20px; top: 20px; position: absolute; border-radius: 5px; justify-content: center; align-items: center; display: inline-flex">
+                        <img style="width: 338px; height: 240px; border-radius: 5px" src="https://via.placeholder.com/338x240" />
+                    </div>
+                    <div style="width: 338px; left: 20px; top: 275px; position: absolute; color: black; font-size: 16px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow</div>
+                    <div style="width: 338px; left: 20px; top: 329px; position: absolute; color: #1D86C5; font-size: 12px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">Saturdat, March 18, 9.30PM</div>
+                    <div style="width: 338px; left: 20px; top: 365px; position: absolute; color: #7E7E7E; font-size: 12px; font-family: Open Sans; font-weight: 400; word-wrap: break-word">ONLINE EVENT - Attend anywhere</div>
+                </div>
+                <div style="left: 0px; top: 0px; position: absolute"><span style="color: black; font-size: 36px; font-family: Open Sans; font-weight: 700; word-wrap: break-word">Upcoming </span><span style="color: #1D86C5; font-size: 36px; font-family: Open Sans; font-weight: 700; word-wrap: break-word">Events</span></div>
+            </div>
+        </div>
+
+        <div class="centered-container">
+            <button class="custom-button">Click me</button>
+        </div>
+
+        <div class="club-container">
+            <h2 class="title"><br>Club</h2>
+            <div class="row">
+                <div class="column">
+                    <img src="https://via.placeholder.com/338x240" />
+                    <h4> Club Name </h4>
+                    <button class="btn my-3" name="add">View</button>
+
+                </div>
+                
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
