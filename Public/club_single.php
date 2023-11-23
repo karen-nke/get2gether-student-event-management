74f2ebb23778caa2c62580930039ef7d6cf52de2
Karen <103889430+karen-nke@users.noreply.github.com>
2023-11-23 15:50:43 +0800

    Single Event Function

--- Public/club_single.php
+++ Public/club_single.php
@@ -23,7 +23,7 @@ require_once('Part/navbar.php');
 
 
 
-.single-event-container .title{
+.single-container .title{
     color: black;
     font-size: 64px;
     font-weight: 700;
@@ -31,14 +31,14 @@ require_once('Part/navbar.php');
     margin-bottom: 25px;
 }
 
-.single-event-container .subscribe{
+.single-container .subscribe{
     color: black;
     font-size: 32px;
     font-weight: 700;
     margin-top:25px;
 }
 
-.single-event-container .count{
+.single-container .count{
     padding-left:20px;
     color: #7E7E7E;
     font-size: 24px;
@@ -47,14 +47,14 @@ require_once('Part/navbar.php');
 
 
 
-.single-event-container .field-name{
+.single-container .field-name{
     color: black;
     font-size: 24px;
     font-weight: 700;
     margin-top: 50px;
 }
 
-.single-event-container .desc{
+.single-container .desc{
 
     color: #7E7E7E;
     font-size: 16px;
@@ -68,41 +68,55 @@ require_once('Part/navbar.php');
 
 <body>
 
-<div class="page-container">
-        <div class="image-container">
-                <img src="Image/Event1.png" class ="image-banner" alt="Communication Badge">
-        </div>
+    <div class="page-container">
+        <?php
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
 
-        
-            <div class= "single-event-container">
-                <h2 class="title">Coding Club</h2>
-                <h2 class ="subscribe"> Subscriber Count <span class ="count">1112</span> </h2>
-                <button class ="btn"> <a href= "#">Subscribe</a></button>
+        require_once('Part/db_controller.php');
 
-                <p class ="field-name">Club Description</p>
-                <p class ="desc"> Explore the world of coding and programming! </p>
+        if (isset($_GET['id']) && !empty($_GET['id'])) {
+            $club_id = $_GET['id'];
 
-                <p class ="field-name">Contact</p>
-                <p class ="desc">coding@example.com</p>
+            // Fetch club details based on the id
+            $sql = "SELECT * FROM clubs WHERE id = $club_id";
+            $result = $conn->query($sql);
 
-                <p class ="field-name">Social Media</p>
-                <a href="#"><p class ="desc"> Instagram</p></a>
-                <a href="#"><p class ="desc"> Faceboook</p></a>
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                // Display club details on this page
+        ?>
+                <div class="image-container">
+                    <!-- Assuming you have a club image path in your database, update the source accordingly -->
+                    <img src="<?php echo htmlspecialchars($row['profile_image']); ?>" class="image-banner" alt="Club Banner">
+                </div>
 
-            </div>
+                <div class="single-container">
+                    <h2 class="title"><?php echo $row['club_name']; ?></h2>
+                    <h2 class="subscribe">Subscriber Count <span class="count"><?php echo $row['subscriber_count']; ?></span></h2>
+                    <button class="btn"><a href="#">Subscribe</a></button>
 
-        
+                    <p class="field-name">Club Description</p>
+                    <p class="desc"><?php echo $row['description']; ?></p>
 
-        </div>
-</div>
+                    <p class="field-name">Contact</p>
+                    <p class="desc"><?php echo $row['contact_email']; ?></p>
 
-        
-  
-    
-       
-               
+                    <p class="field-name">Social Media</p>
+                    <a href="<?php echo $row['instagram_link']; ?>"><p class="desc">Instagram</p></a>
+                    <a href="<?php echo $row['facebook_link']; ?>"><p class="desc">Facebook</p></a>
+                </div>
 
-                        
+        <?php
+            } else {
+                echo "Club not found";
+            }
+        } else {
+            echo "No club selected";
+        }
+        ?>
+    </div>
+
 </body>
 
 </html>
