25de295dbf7af58a6e75825b78e6f8d3be589c02
Karen <103889430+karen-nke@users.noreply.github.com>
2023-11-29 14:48:25 +0800

    Member Count

--- Public/club_single.php
+++ Public/club_single.php
@@ -5,7 +5,6 @@ ini_set('display_errors', 1);
 
 session_start();
 
-
 require_once('Part/db_controller.php');
 require_once('Part/navbar.php');
 
@@ -16,6 +15,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['user_id'])) {
 
 }
 
+//Club Details 
 if (isset($_GET['id']) && !empty($_GET['id'])) {
     $club_id = $_GET['id'];
 
@@ -33,6 +33,32 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
 } else {
     echo "No club selected";
 }
+
+//Club Event
+
+$club_id = $row['id']; 
+$sql = "SELECT events.*, clubs.club_name FROM events
+        JOIN clubs ON events.club_id = clubs.id
+        WHERE events.club_id = $club_id AND events.start_date >= CURDATE()
+        ORDER BY events.start_date LIMIT 3";
+
+$result = $conn->query($sql);
+
+//Club Member
+$sql = "SELECT clubs.*, COUNT(memberships.user_id) AS member_count
+        FROM clubs
+        LEFT JOIN memberships ON clubs.id = memberships.club_id
+        WHERE clubs.id = $club_id";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    // Get the count of members for this club
+    $member_count = $row['member_count'];
+} else {
+    echo "Club not found";
+}
+
 
 function fetchUserRole($user_id, $club_id) {
     global $conn;
@@ -116,14 +142,16 @@ $userRole = fetchUserRole($userId, $club_id);
 
     <div class="page-container">
             <div class="image-container">
-                    <!-- Assuming you have a club image path in your database, update the source accordingly -->
                     <img src="<?php echo htmlspecialchars($row['profile_image']); ?>" class="image-banner" alt="Club Banner">
             </div>
 
                 <div class="single-container">
+
                     <h2 class="title"><?php echo $row['club_name']; ?></h2>
-                    <h2 class="subscribe">Subscriber Count <span class="count"><?php echo $row['subscriber_count']; ?></span></h2>
+
+                    <h2 class="subscribe">Member Count <span class="count"><?php echo $member_count ?></span></h2>
                     <a href="#"><button class="btn">Subscribe</button></a>
+
                     <form method="post" action="join_club.php">
                         <input type="hidden" name="club_id" value="<?php echo $club_id; ?>">
                         <button type="submit" class="btn" name="join_club">Join Club</button>
@@ -153,36 +181,24 @@ $userRole = fetchUserRole($userId, $club_id);
                     <div class="event-container">
 
                         <?php
-                        error_reporting(E_ALL);
-                        ini_set('display_errors', 1);
-
-                        
-                        $club_id = $row['id']; 
-                        $sql = "SELECT events.*, clubs.club_name FROM events
-                                JOIN clubs ON events.club_id = clubs.id
-                                WHERE events.club_id = $club_id AND events.start_date >= CURDATE()
-                                ORDER BY events.start_date LIMIT 3";
-
-                        $result = $conn->query($sql);
-
-                        if ($result->num_rows > 0) {
-                            echo '<div class="event-container">';
-                            // Output data of each row
-                            while ($row = $result->fetch_assoc()) {
-                                echo '<a href="event_single.php?id=' . $row["id"] . '">';
-                                echo '<div class="event-card">';
-                                echo '<img src="' . htmlspecialchars($row["event_image_path"]) . '" alt="Event Image">';
-                                echo '<h2 class="title">' . $row["event_title"] . '</h2>';
-                                echo '<p class="date">Date & Time: ' . $row["start_date"] . '</p>';
-                                echo '<p class="location">Location: ' . $row["event_venue"] . '</p>';
-                                echo '<p class="location">Club: ' . $row["club_name"] . '</p>';
+                            if ($result->num_rows > 0) {
+                                echo '<div class="event-container">';
+                                // Output data of each row
+                                while ($row = $result->fetch_assoc()) {
+                                    echo '<a href="event_single.php?id=' . $row["id"] . '">';
+                                    echo '<div class="event-card">';
+                                    echo '<img src="' . htmlspecialchars($row["event_image_path"]) . '" alt="Event Image">';
+                                    echo '<h2 class="title">' . $row["event_title"] . '</h2>';
+                                    echo '<p class="date">Date & Time: ' . $row["start_date"] . '</p>';
+                                    echo '<p class="location">Location: ' . $row["event_venue"] . '</p>';
+                                    echo '<p class="location">Club: ' . $row["club_name"] . '</p>';
+                                    echo '</div>';
+                                    echo '</a>';
+                                }
                                 echo '</div>';
-                                echo '</a>';
+                            } else {
+                                echo "No upcoming events for this club.";
                             }
-                            echo '</div>';
-                        } else {
-                            echo "No upcoming events for this club.";
-                        }
                         ?>
 
                     </div>
