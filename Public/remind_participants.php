c33999d239abd688e7681a230d893eeaf558e9e5
Karen <103889430+karen-nke@users.noreply.github.com>
2023-12-19 15:24:46 +0800

    Add Time Constraint

--- Public/remind_participants.php
+++ Public/remind_participants.php
@@ -23,51 +23,59 @@ if (isset($_SESSION['username']) && isset($_SESSION['user_id'])) {
         if ($event) {
             $eventTitle = $event['event_title'];
             $clubId = $event['club_id'];
+            $eventStartDate = strtotime($event['start_date']);
 
-            $club = fetchClubDetails($clubId, $conn);
+            // Check if the current date is one day before the event's start date
+            if (strtotime('tomorrow') === $eventStartDate - 86400) {
+                $club = fetchClubDetails($clubId, $conn);
 
-            if ($club) {
-                $clubName = $club['club_name'];
+                if ($club) {
+                    $clubName = $club['club_name'];
 
-                // Fetch registered participants
-                $participantsSql = "SELECT users.id, users.username FROM event_registrations
-                                    JOIN users ON event_registrations.user_id = users.id
-                                    WHERE event_registrations.event_id = $event_id";
-                $participantsResult = $conn->query($participantsSql);
+                    // Fetch registered participants
+                    $participantsSql = "SELECT users.id, users.username FROM event_registrations
+                                        JOIN users ON event_registrations.user_id = users.id
+                                        WHERE event_registrations.event_id = $event_id";
+                    $participantsResult = $conn->query($participantsSql);
 
-                if ($participantsResult) {
-                    if ($participantsResult->num_rows > 0) {
-                        // Send reminder notifications
-                        while ($participant = $participantsResult->fetch_assoc()) {
-                            $userId = $participant['id'];
-                            $username = $participant['username'];
-                        
-                            $reminderMessage = "Reminder: Your registered Event '{$eventTitle}' with '{$clubName}' is starting tomorrow, see you soon!";
-                            $escapedReminderMessage = mysqli_real_escape_string($conn, $reminderMessage);
-                        
-                            $reminderInsertSql = "INSERT INTO notifications (user_id, message, is_read) VALUES ($userId, '$escapedReminderMessage', 0)";
-                        
-                            if (mysqli_query($conn, $reminderInsertSql)) {
-                                // Successful query
-                            } else {
-                                echo "Error sending reminder for user '$username': " . mysqli_error($conn);
-                            
+                    if ($participantsResult) {
+                        if ($participantsResult->num_rows > 0) {
+                            // Send reminder notifications
+                            while ($participant = $participantsResult->fetch_assoc()) {
+                                $userId = $participant['id'];
+                                $username = $participant['username'];
+
+                                $reminderMessage = "Reminder: Your registered Event '{$eventTitle}' with '{$clubName}' is starting tomorrow, see you soon!";
+                                $escapedReminderMessage = mysqli_real_escape_string($conn, $reminderMessage);
+
+                                $reminderInsertSql = "INSERT INTO notifications (user_id, message, is_read) VALUES ($userId, '$escapedReminderMessage', 0)";
+
+                                if (mysqli_query($conn, $reminderInsertSql)) {
+                                    // Successful query
+                                } else {
+                                    echo "Error sending reminder for user '$username': " . mysqli_error($conn);
+                                }
                             }
-                        }
 
-                        // Display confirmation alert
-                        echo "<script>
-                                alert('Reminder Sent!');
-                                window.history.back();
-                            </script>";
+                            // Display confirmation alert
+                            echo "<script>
+                                    alert('Reminder Sent!');
+                                    window.history.back();
+                                </script>";
+                        } else {
+                            echo "No participants registered for this event.";
+                        }
                     } else {
-                        echo "No participants registered for this event.";
+                        echo "Error fetching participants: " . $conn->error;
                     }
                 } else {
-                    echo "Error fetching participants: " . $conn->error;
+                    echo "Error fetching club details: Club not found.";
                 }
             } else {
-                echo "Error fetching club details: Club not found.";
+                echo "<script>
+                        alert('You can only remind participants a day before the event.');
+                        window.history.back();
+                    </script>";
             }
         } else {
             echo "Error fetching event details: Event not found.";
