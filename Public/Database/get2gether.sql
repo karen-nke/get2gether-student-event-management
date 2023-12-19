51ab0340e32f57fe487e84b77c4bf1ec68cf02cb
Karen <103889430+karen-nke@users.noreply.github.com>
2023-12-19 16:50:43 +0800

    Fixed Date and Database

--- Public/Database/get2gether.sql
+++ Public/Database/get2gether.sql
@@ -117,14 +117,14 @@ CREATE TABLE `events` (
 
 INSERT INTO `events` (`id`, `event_title`, `event_venue`, `start_time`, `end_time`, `start_date`, `end_date`, `event_image_path`, `event_description`, `club_id`, `user_id`) VALUES
 (1, 'Annual Charity Gala', 'Grand Ballroom, XYZ Hotel', '18:00:00', '23:00:00', '2023-12-23', '2023-12-23', 'Image/Event1.png', 'Join us for an elegant evening of fundraising and entertainment.', 4, 4),
-(2, 'Tech Symposium 2023', 'Conference Center, Innovation Hub', '09:00:00', '17:00:00', '2024-12-22', '2024-12-22', 'Image/Event2.png', 'Explore the latest trends and innovations in technology.', 1, 1),
+(2, 'Tech Symposium 2023', 'Conference Center, Innovation Hub', '09:00:00', '17:00:00', '2024-12-21', '2024-12-21', 'Image/Event2.png', 'Explore the latest trends and innovations in technology.', 1, 1),
 (3, 'Art Exhibition: Visionaries', 'Art Gallery, City Museum', '11:00:00', '18:00:00', '2024-02-24', '2024-02-24', 'Image/Event3.png', 'A showcase of visionary artists pushing the boundaries of creativity.', 4, 4),
 (4, 'Food Festival: Taste of the World', 'Outdoor Plaza, Downtown', '15:00:00', '22:00:00', '2023-12-25', '2023-12-25', 'Image/Event4.png', 'Savor delicious dishes from around the globe in one place.', 4, 4),
 (5, 'Startup Networking Mixer', 'Co-Working Space, Tech Hub', '17:30:00', '20:30:00', '2023-12-10', '2023-12-10', 'Image/Event5.png', 'Connect with fellow entrepreneurs and investors in the startup ecosystem.', 6, 6),
 (6, 'Music Festival: Harmony Beats', 'Main Stage, City Park', '12:00:00', '22:00:00', '2023-12-27', '2023-12-27', 'Image/Event6.png', 'Experience a day filled with diverse musical performances and positive vibes.', 4, 4),
 (7, 'Beach Cleaning', 'Testing', '16:19:00', '18:20:00', '2023-12-28', '2023-12-28', 'uploads/beach cleaning.png', 'Testing', 5, 5),
 (8, 'Tech Expo 2024', 'Innovation Hall', '12:00:00', '14:00:00', '2023-12-29', '2023-12-29', 'Image/Event1.png', 'Explore the latest technological innovations and trends.', 6, 6),
-(9, 'Code Jam Championship', 'Coding Arena', '15:00:00', '17:00:00', '2023-12-21', '2023-12-21', 'Image/Event2.png', 'Compete in a coding challenge and showcase your skills.', 1, 1),
+(9, 'Code Jam Championship', 'Coding Arena', '15:00:00', '17:00:00', '2023-12-20', '2023-12-20', 'Image/Event2.png', 'Compete in a coding challenge and showcase your skills.', 1, 1),
 (10, 'Startup Networking Night', 'Entrepreneurship Hub', '18:00:00', '20:00:00', '2023-12-31', '2023-12-31', 'Image/Event3.png', 'Connect with fellow entrepreneurs and investors.', 6, 6),
 (11, 'Photography Exhibition', 'Art Gallery', '13:00:00', '15:00:00', '2024-01-01', '2024-01-01', 'Image/Event4.png', 'Showcasing stunning photography from our club members.', 2, 2),
 (12, 'Camera Techniques Workshop', 'Studio A', '16:00:00', '18:00:00', '2024-01-02', '2024-01-02', 'Image/Event5.png', 'Learn advanced photography techniques from experts.', 2, 2),
@@ -132,6 +132,7 @@ INSERT INTO `events` (`id`, `event_title`, `event_venue`, `start_time`, `end_tim
 (14, 'Sports Fest 2024', 'Sports Complex', '14:00:00', '16:00:00', '2024-01-04', '2024-01-04', 'Image/Event1.png', 'An exciting day of sports competitions and games.', 3, 3),
 (15, 'Fitness Challenge Day', 'Gymnasium', '17:00:00', '19:00:00', '2024-01-05', '2024-01-05', 'Image/Event2.png', 'Join us for a day of fitness challenges and workouts.', 5, 5),
 (16, 'Soccer Tournament', 'Soccer Field', '20:00:00', '22:00:00', '2024-01-06', '2024-01-06', 'Image/Event3.png', 'Compete in our annual soccer tournament.', 3, 3);
+(17, 'Tech Symposium 2023', 'Conference Center, Innovation Hub', '09:00:00', '17:00:00', '2024-12-21', '2024-12-21', 'Image/Event2.png', 'Explore the latest trends and innovations in technology.', 1, 7),
 
 
 
